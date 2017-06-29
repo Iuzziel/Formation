703bc3c5ac4cc275dd63b7b8dfe0b9c97995437e
Iuzziel <Iuzziel@users.noreply.github.com>
2017-06-29 10:39:51 +0200

    Create Principale.java

---
+++ 503_ConvertisseurCFahr/src/application/Principale.java
@@ -0,0 +1,11 @@
+package application;
+
+import fenetres.Fenetre;
+
+public class Principale{
+	//*********************************Début de la Main*********************************//
+	public static void main (String [] args){
+		Fenetre fenetre = new Fenetre();
+		fenetre.setVisible(true);
+	}
+}
