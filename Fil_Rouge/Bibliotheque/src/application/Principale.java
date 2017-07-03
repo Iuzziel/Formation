a696656b40641bb22bb421e84804bdbc46b5fb16
Iuzziel <Iuzziel@users.noreply.github.com>
2017-07-03 13:33:10 +0200

    Create Principale.java

---
+++ Fil_Rouge/Bibliotheque/src/application/Principale.java
@@ -0,0 +1,11 @@
+package application;
+
+import fenetres.*;
+
+public class Principale{
+	//*********************************Début de la Main*********************************//
+	public static void main (String [] args){
+		FenetreClient fenetre = new FenetreClient();
+		fenetre.setVisible(true);
+	}
+}
