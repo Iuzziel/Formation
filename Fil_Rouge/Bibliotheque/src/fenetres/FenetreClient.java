3aeda3d6ffddeb35f5d23cec1ee7f65330223ec9
Iuzziel <Iuzziel@users.noreply.github.com>
2017-07-03 15:52:00 +0200

    Update FenetreClient.java

--- Fil_Rouge/Bibliotheque/src/fenetres/FenetreClient.java
+++ Fil_Rouge/Bibliotheque/src/fenetres/FenetreClient.java
@@ -12,7 +12,7 @@ public class FenetreClient extends JFrame{
 	private static final long serialVersionUID = -629171735000729382L;
 
 	public FenetreClient() {
-		setMinimumSize(new Dimension(800, 600));
+		this.setMinimumSize(new Dimension(800, 600));
 		this.setSize(800, 600);
 		this.setTitle("Bienvenu dans le logiciel de la bibliothèque");
 		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
@@ -21,7 +21,7 @@ public class FenetreClient extends JFrame{
 		this.getContentPane().setLayout(new BorderLayout(5, 5));
 		RechercherUnLivre moduleRecherche = new RechercherUnLivre();
 		InformationLivre moduleInformation = new InformationLivre();
-		Header moduleHeader = new Header();
+		Header moduleHeader = new Header("Espace visiteurs");
 		this.getContentPane().add(moduleHeader, BorderLayout.NORTH);
 		this.getContentPane().add(moduleRecherche, BorderLayout.WEST);
 		this.getContentPane().add(moduleInformation, BorderLayout.CENTER);
