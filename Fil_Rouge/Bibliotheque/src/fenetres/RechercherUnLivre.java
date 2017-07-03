f391cc2d99bcef33318ceeac873f4a2bd81d388f
Iuzziel <Iuzziel@users.noreply.github.com>
2017-07-03 16:56:33 +0200

    Update RechercherUnLivre.java

--- Fil_Rouge/Bibliotheque/src/fenetres/RechercherUnLivre.java
+++ Fil_Rouge/Bibliotheque/src/fenetres/RechercherUnLivre.java
@@ -1,6 +1,5 @@
 package fenetres;
 
-import java.awt.GridLayout;
 import javax.swing.JPanel;
 import javax.swing.border.TitledBorder;
 import javax.swing.JLabel;
@@ -9,87 +8,88 @@ import javax.swing.JButton;
 import javax.swing.JTable;
 import java.awt.Dimension;
 import java.awt.BorderLayout;
-import java.awt.FlowLayout;
 import javax.swing.JScrollPane;
+import javax.swing.BoxLayout;
 
 public class RechercherUnLivre extends JPanel{
-	
+
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = -3793794312904965025L;
-	
+
 	//Données membre //TODO Penser à changer avec getter et setter dans le code final
 	private JTextField textFieldTitre;
 	private JTextField textFieldAuteur;
 	private JTextField textFieldTheme;
 	private JTable tabRenvoiResultatsLivre;
-	
+
 	//Constructeur
 	public RechercherUnLivre() {
 		setBorder(new TitledBorder(null, "Rechercher un ouvrage", TitledBorder.LEADING, TitledBorder.TOP, null, null));
-		setLayout(new GridLayout(0, 1, 5, 5));
-		
+		setLayout(new BoxLayout(this, BoxLayout.X_AXIS));
+
 		JPanel panRechercheResultats = new JPanel();
 		add(panRechercheResultats);
-		panRechercheResultats.setLayout(new GridLayout(2, 1, 5, 5));
-		
+		panRechercheResultats.setLayout(new BoxLayout(panRechercheResultats, BoxLayout.Y_AXIS));
+
 		//////////////////////////////////////Panel de la recherche de livre//////////////////////////////////////
 		JPanel panRecherche = new JPanel();
 		panRechercheResultats.add(panRecherche);
-		panRecherche.setLayout(new GridLayout(4, 1, 5, 5));
-		
+		panRecherche.setLayout(new BoxLayout(panRecherche, BoxLayout.Y_AXIS));
+
 		//panRechercheLabel
 		JPanel panRechercheTitre = new JPanel();
 		panRecherche.add(panRechercheTitre);
-		panRechercheTitre.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
-		JLabel lblTitre = new JLabel("Titre");
+		panRechercheTitre.setLayout(new BoxLayout(panRechercheTitre, BoxLayout.X_AXIS));
+		JLabel lblTitre = new JLabel("Titre : ");
 		panRechercheTitre.add(lblTitre);
 		textFieldTitre = new JTextField();
 		panRechercheTitre.add(textFieldTitre);
 		textFieldTitre.setColumns(10);
-		
+
 		JPanel panRechercheAuteur = new JPanel();
 		panRechercheAuteur.setMaximumSize(new Dimension(600, 35));
 		panRecherche.add(panRechercheAuteur);
-		panRechercheAuteur.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
-		JLabel lblAuteur = new JLabel("Auteur");
+		panRechercheAuteur.setLayout(new BoxLayout(panRechercheAuteur, BoxLayout.X_AXIS));
+		JLabel lblAuteur = new JLabel("Auteur : ");
 		panRechercheAuteur.add(lblAuteur);
 		textFieldAuteur = new JTextField();
 		panRechercheAuteur.add(textFieldAuteur);
 		textFieldAuteur.setColumns(10);
-		
+
 		JPanel panRechercheTheme = new JPanel();
 		panRecherche.add(panRechercheTheme);
-		panRechercheTheme.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
-		JLabel lblTheme = new JLabel("Theme");
+		panRechercheTheme.setLayout(new BoxLayout(panRechercheTheme, BoxLayout.X_AXIS));
+		JLabel lblTheme = new JLabel("Th\u00E8me : ");
 		panRechercheTheme.add(lblTheme);
 		textFieldTheme = new JTextField();
 		panRechercheTheme.add(textFieldTheme);
 		textFieldTheme.setColumns(10);
-		
+
 		JPanel panRechercheBoutton = new JPanel();
 		panRecherche.add(panRechercheBoutton);
+		panRechercheBoutton.setLayout(new BoxLayout(panRechercheBoutton, BoxLayout.X_AXIS));
 		JButton btnRechercherLivre = new JButton("Rechercher");
 		panRechercheBoutton.add(btnRechercherLivre);
-		
+
 		//////////////////////////////////////Panel des resultats//////////////////////////////////////
 		JPanel panResultats = new JPanel();
 		panRechercheResultats.add(panResultats);
 		panResultats.setLayout(new BorderLayout(5, 5));
-		
+
 		JButton btnResultatsPlusInfos = new JButton("+ d'infos");
 		panResultats.add(btnResultatsPlusInfos, BorderLayout.SOUTH);
-		
+
 		JLabel lblRenvoiResultatsTitre = new JLabel("Livre(s) correspondant(s) :");
 		panResultats.add(lblRenvoiResultatsTitre, BorderLayout.NORTH);
-		
-		
+
+
 		//TODO Remplir la JTable avec 2 vectors
 		tabRenvoiResultatsLivre = new JTable();
-		tabRenvoiResultatsLivre.setPreferredSize(new Dimension(50, 60));
+		tabRenvoiResultatsLivre.setPreferredSize(new Dimension(250, 150));
 		panResultats.add(tabRenvoiResultatsLivre, BorderLayout.CENTER);
-		
+
 		JScrollPane srlTabRenvoiResultatsLivre = new JScrollPane(tabRenvoiResultatsLivre);
 		panResultats.add(srlTabRenvoiResultatsLivre, BorderLayout.WEST);
 
