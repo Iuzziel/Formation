348a71ef9d2c72b8ced7e822f7fe8bacba36123c
Iuzziel <Iuzziel@users.noreply.github.com>
2017-07-05 13:14:43 +0200

    Update FenetreClient.java

--- Fil_Rouge/Bibliotheque/src/fenetres/FenetreClient.java
+++ Fil_Rouge/Bibliotheque/src/fenetres/FenetreClient.java
@@ -1,30 +1,30 @@
 package fenetres;
 
 import javax.swing.JFrame;
-
 import panneaux.Header;
 import panneaux.InformationLivre;
 import panneaux.RechercherUnLivre;
-
 import java.awt.BorderLayout;
 import java.awt.Dimension;
 
 public class FenetreClient extends JFrame{
-	
+
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = -629171735000729382L;
 
+
+	//Constructeur de la fenetre
 	public FenetreClient() {
 		this.setMinimumSize(new Dimension(800, 600));
-		this.setSize(800, 600);
+		this.setSize(1024, 768);
 		this.setTitle("Bienvenu dans le logiciel de la bibliothèque");
 		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
 		this.setLocationRelativeTo(null);
 		this.setResizable(true);
 		this.getContentPane().setLayout(new BorderLayout(5, 5));
-		
+
 		//Création des modules composant cette Frame
 		RechercherUnLivre moduleRechercheLivre = new RechercherUnLivre();
 		moduleRechercheLivre.setMaximumSize(new Dimension(500, 2147483647));
@@ -32,11 +32,22 @@ public class FenetreClient extends JFrame{
 		moduleRechercheLivre.setMinimumSize(new Dimension(150, 200));
 		InformationLivre moduleInformationLivre = new InformationLivre();
 		Header moduleHeader = new Header("Espace visiteurs");
-		
+
 		this.getContentPane().add(moduleHeader, BorderLayout.NORTH);
 		this.getContentPane().add(moduleRechercheLivre, BorderLayout.WEST);
 		this.getContentPane().add(moduleInformationLivre, BorderLayout.CENTER);
 
+		/* TODO Mettre non editable avec les getter depuis le moduleRechercheLivre
+		JTextField textFieldTitre;
+	 	JTextField textFieldAuteur;
+	 	JTextField textFieldTheme;
+	 	JTextField textFieldEmplacement;
+	 	JTextField textFieldISBN;
+	 	JTextField textFieldISSN;
+	 	JTextField textFieldNbExemplaireDispo;
+	 	JTextField textFieldNbExemplaireDispoBiblio;
+	 	JTextArea txtAreaComment;
+		 */
 	}
 
 }
