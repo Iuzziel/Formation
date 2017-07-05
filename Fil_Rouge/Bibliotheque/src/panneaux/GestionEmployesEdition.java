728be0e7b0a39553f1675e689966d9298abbb6ed
Iuzziel <Iuzziel@users.noreply.github.com>
2017-07-05 13:16:49 +0200

    Update GestionEmployesEdition.java

--- Fil_Rouge/Bibliotheque/src/panneaux/GestionEmployesEdition.java
+++ Fil_Rouge/Bibliotheque/src/panneaux/GestionEmployesEdition.java
@@ -7,6 +7,7 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.border.TitledBorder;
 import java.awt.GridLayout;
+import javax.swing.JTextField;
 
 public class GestionEmployesEdition extends JPanel{
 
@@ -15,36 +16,150 @@ public class GestionEmployesEdition extends JPanel{
 	 */
 	private static final long serialVersionUID = 2902920964663284927L;
 
-	public GestionEmployesEdition() {
-		this.setPreferredSize(new Dimension(250, 150));
-		this.setBorder(new TitledBorder(null, "Affichage des informations détaillées de l'employé sélectionné", TitledBorder.LEADING, TitledBorder.TOP));
-		setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+	//Données membres
+	private JTextField txtFieldNom;
+	private JTextField txtFieldPrenom;
+	private JTextField txtFieldAdresse;
+	private JTextField txtFieldTelephone;
+	private JTextField txtFieldService;
+	private JTextField txtFieldRole;
 
-		//Création d'un panel pour les label infos
-		JPanel panLblInfo = new JPanel();
-		add(panLblInfo);
-		panLblInfo.setLayout(new GridLayout(0, 1, 0, 0));
-		//Affichage des information de l'employé
+	//Constructeur
+	public GestionEmployesEdition() {
+		this.setPreferredSize(new Dimension(350, 200));
+		this.setBorder(new TitledBorder(null, "Affichage des informations détaillées de l'employé sélectionné", TitledBorder.LEADING, TitledBorder.TOP));
+		this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+
+		//Création d'un panel pour les infos
+		JPanel panInfo = new JPanel();
+		add(panInfo);
+		panInfo.setLayout(new GridLayout(0, 1, 2, 2));
+
+		//Création d'un panel pour le Nom + textFieldNom
+		JPanel panInfoNom = new JPanel();
+		panInfo.add(panInfoNom);
+		panInfoNom.setLayout(new BoxLayout(panInfoNom, BoxLayout.X_AXIS));
 		JLabel lblNom = new JLabel("Nom : ");
-		panLblInfo.add(lblNom);
+		panInfoNom.add(lblNom);
+		txtFieldNom = new JTextField();
+		txtFieldNom.setMaximumSize(new Dimension(2147483647, 20));
+		panInfoNom.add(txtFieldNom);
+		txtFieldNom.setColumns(10);
+
+		//Création d'un panel pour le prénom + textFieldPrenom
+		JPanel panInfoPrenom = new JPanel();
+		panInfo.add(panInfoPrenom);
+		panInfoPrenom.setLayout(new BoxLayout(panInfoPrenom, BoxLayout.X_AXIS));
 		JLabel lblPrenom = new JLabel("Prénom : ");
-		panLblInfo.add(lblPrenom);
+		panInfoPrenom.add(lblPrenom);
+		txtFieldPrenom = new JTextField();
+		txtFieldPrenom.setMaximumSize(new Dimension(2147483647, 20));
+		panInfoPrenom.add(txtFieldPrenom);
+		txtFieldPrenom.setColumns(10);
+
+		//Création d'un panel pour l'Adresse + textFieldAdresse
+		JPanel panInfoAdresse = new JPanel();
+		panInfo.add(panInfoAdresse);
+		panInfoAdresse.setLayout(new BoxLayout(panInfoAdresse, BoxLayout.X_AXIS));
 		JLabel lblAdresse = new JLabel("Adresse : ");
-		panLblInfo.add(lblAdresse);
+		panInfoAdresse.add(lblAdresse);
+		txtFieldAdresse = new JTextField();
+		txtFieldAdresse.setMaximumSize(new Dimension(2147483647, 20));
+		panInfoAdresse.add(txtFieldAdresse);
+		txtFieldAdresse.setColumns(10);
+
+		//Création d'un panel pour le numéroTel + textFieldTelephone
+		JPanel panInfoTel = new JPanel();
+		panInfo.add(panInfoTel);
+		panInfoTel.setLayout(new BoxLayout(panInfoTel, BoxLayout.X_AXIS));
 		JLabel lblTelephone = new JLabel("Téléphone : ");
-		panLblInfo.add(lblTelephone);
+		panInfoTel.add(lblTelephone);
+		txtFieldTelephone = new JTextField();
+		txtFieldTelephone.setMaximumSize(new Dimension(2147483647, 20));
+		panInfoTel.add(txtFieldTelephone);
+		txtFieldTelephone.setColumns(10);
+
+		//Création d'un panel pour le Service + textFieldService
+		JPanel panInfoService = new JPanel();
+		panInfo.add(panInfoService);
+		panInfoService.setLayout(new BoxLayout(panInfoService, BoxLayout.X_AXIS));
 		JLabel lblService = new JLabel("Service : ");
-		panLblInfo.add(lblService);
+		panInfoService.add(lblService);
+		txtFieldService = new JTextField();
+		txtFieldService.setMaximumSize(new Dimension(2147483647, 20));
+		panInfoService.add(txtFieldService);
+		txtFieldService.setColumns(10);
+
+		//Création d'un panel pour le Role + textFieldRole
+		JPanel panInfoRole = new JPanel();
+		panInfo.add(panInfoRole);
+		panInfoRole.setLayout(new BoxLayout(panInfoRole, BoxLayout.X_AXIS));
 		JLabel lblRoleOccupe = new JLabel("Rôle occupé : ");
-		panLblInfo.add(lblRoleOccupe);
+		panInfoRole.add(lblRoleOccupe);
+		txtFieldRole = new JTextField();
+		txtFieldRole.setMaximumSize(new Dimension(2147483647, 20));
+		panInfoRole.add(txtFieldRole);
+		txtFieldRole.setColumns(10);
 
 		//Ajout des boutons
 		JPanel panBtn = new JPanel();
-		panBtn.setLayout(new BoxLayout(panBtn, BoxLayout.X_AXIS));
 		JButton btnEditer = new JButton("Editer cet employé");
+		JButton btnAnnuler = new JButton("Annuler");
 		JButton btnSupprimer = new JButton("Supprimer cet employé");
+		panBtn.setLayout(new BoxLayout(panBtn, BoxLayout.X_AXIS));
 		panBtn.add(btnEditer);
+		panBtn.add(btnAnnuler);
 		panBtn.add(btnSupprimer);
 		this.add(panBtn);
 	}
+
+	//Accesseurs
+	public JTextField getTxtFieldNom() {
+		return txtFieldNom;
+	}
+
+	public void setTxtFieldNom(JTextField txtFieldNom) {
+		this.txtFieldNom = txtFieldNom;
+	}
+
+	public JTextField getTxtFieldPrenom() {
+		return txtFieldPrenom;
+	}
+
+	public void setTxtFieldPrenom(JTextField txtFieldPrenom) {
+		this.txtFieldPrenom = txtFieldPrenom;
+	}
+
+	public JTextField getTxtFieldAdresse() {
+		return txtFieldAdresse;
+	}
+
+	public void setTxtFieldAdresse(JTextField txtFieldAdresse) {
+		this.txtFieldAdresse = txtFieldAdresse;
+	}
+
+	public JTextField getTxtFieldTelephone() {
+		return txtFieldTelephone;
+	}
+
+	public void setTxtFieldTelephone(JTextField txtFieldTelephone) {
+		this.txtFieldTelephone = txtFieldTelephone;
+	}
+
+	public JTextField getTxtFieldService() {
+		return txtFieldService;
+	}
+
+	public void setTxtFieldService(JTextField txtFieldService) {
+		this.txtFieldService = txtFieldService;
+	}
+
+	public JTextField getTxtFieldRole() {
+		return txtFieldRole;
+	}
+
+	public void setTxtFieldRole(JTextField txtFieldRole) {
+		this.txtFieldRole = txtFieldRole;
+	}
+
 }
