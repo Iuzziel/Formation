d202db5bd7f404803e35e092eb959e8486cb70ce
Iuzziel <Iuzziel@users.noreply.github.com>
2017-07-05 13:15:16 +0200

    Update FenetreEmploye.java

--- Fil_Rouge/Bibliotheque/src/fenetres/FenetreEmploye.java
+++ Fil_Rouge/Bibliotheque/src/fenetres/FenetreEmploye.java
@@ -15,10 +15,11 @@ public class FenetreEmploye extends JFrame {
 	 */
 	private static final long serialVersionUID = -6154275497867212254L;
 
+	//Constructeur de la fenetre
 	public FenetreEmploye() {
 		this.setTitle("Bienvenu dans le logiciel de la bibliothèque");
 		this.setMinimumSize(new Dimension(800, 600));
-		this.setSize(800, 600);
+		this.setSize(1024, 768);
 		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
 		this.setLocationRelativeTo(null);
 		this.setResizable(true);
@@ -33,7 +34,7 @@ public class FenetreEmploye extends JFrame {
 		//Création des onglets
 		//Création des onglets/tabGestEmprunt
 		JPanel tabGestEmprunt = new JPanel();
-		tabGestEmprunt.setLayout(new GridLayout(2, 2, 5, 5));
+		tabGestEmprunt.setLayout(new GridLayout(0, 1, 5, 5));
 		tabEspaceEmploye.addTab("Gestion des emprunts", null, tabGestEmprunt, null);
 
 		//Création des onglets/tabGestFond
@@ -50,23 +51,52 @@ public class FenetreEmploye extends JFrame {
 		tabAdmin.setLayout(new GridLayout(2, 2, 5, 5));
 		tabEspaceEmploye.addTab("Gestion des comptes employ\u00E9s", null, tabAdmin, null);
 
+
 		///////////////////////////////////////////////TODO Remplissage des onglets///////////////////////////////////////////////
-		//Tab gestion des emprunts
-		tabGestEmprunt.add(new RechercherUnLivre());
-		tabGestEmprunt.add(new InformationLivre());
-		tabGestEmprunt.add(new RechercherUnAdherent());
-		tabGestEmprunt.add(new EmpruntRetour());
+		//*********************************************Tab gestion des emprunts*************************************************//
+		//Création Panel du haut
+		JPanel panTabGestEmpruntHaut = new JPanel();
+		tabGestEmprunt.add(panTabGestEmpruntHaut);
+		panTabGestEmpruntHaut.setLayout(new BorderLayout(0, 0));
 
-		//Tab gestion du fond
-		tabGestFond.add(new RechercherUnLivre());
-		tabGestFond.add(new InformationLivre());
+		//PanHaut Ajout du module recherche
+		RechercherUnLivre rechercherUnLivre = new RechercherUnLivre();
+		panTabGestEmpruntHaut.add(rechercherUnLivre, BorderLayout.WEST);
+		//PanHaut Ajout du module Information Livre
+		InformationLivre gestEmpruntInformationLivre = new InformationLivre();
+		gestEmpruntInformationLivre.setPreferredSize(new Dimension(300, 300));
+		gestEmpruntInformationLivre.setMinimumSize(new Dimension(300, 300));
+		panTabGestEmpruntHaut.add(gestEmpruntInformationLivre, BorderLayout.CENTER);
+		gestEmpruntInformationLivre.setCommentaireEditable(true);
 
-		//Tab gestion du stock
+		//Création Panel du bas
+		JPanel panTabGestEmpruntBas = new JPanel();
+		tabGestEmprunt.add(panTabGestEmpruntBas);
+		panTabGestEmpruntBas.setLayout(new BorderLayout(0, 0));
+		//PanBas Ajout du module recherche adhérent
+		RechercherUnAdherent rechercherUnAdherent = new RechercherUnAdherent();
+		panTabGestEmpruntBas.add(rechercherUnAdherent, BorderLayout.CENTER);
+		//PanBas Ajout du module Emprunt/Retour
+		EmpruntRetour empruntRetour = new EmpruntRetour();
+		panTabGestEmpruntBas.add(empruntRetour, BorderLayout.EAST);
+
+		//*********************************************Tab gestion du fond*************************************************//
+		//Création Panel
+		RechercherUnLivre gestFondRechercherUnLivre = new RechercherUnLivre();
+		gestFondRechercherUnLivre.addBoutonCreerLivre();
+		tabGestFond.add(gestFondRechercherUnLivre);
+		InformationLivre gestFondInformationLivre = new InformationLivre();
+		gestFondInformationLivre.setCommentaireEditable(true);
+		tabGestFond.add(gestFondInformationLivre);
+
+		//*********************************************Tab gestion du stock*************************************************//
+		//Création Panel
 		tabGestStock.setLayout(new BorderLayout(5, 5));
 		tabGestStock.add(new GestionStock(), BorderLayout.WEST);
 		tabGestStock.add(new GestionStockResultAffich(), BorderLayout.CENTER);
 
-		//Tab Admin
+		//*********************************************Tab Admin*************************************************//
+		//Création Panel
 		tabAdmin.setLayout(new BorderLayout(5, 5));
 		tabAdmin.add(new GestionEmployes(), BorderLayout.WEST);
 		tabAdmin.add(new GestionEmployesResultAffich(), BorderLayout.CENTER);
