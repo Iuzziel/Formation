a96c0152c0d1e62a67e664a026d9cad21dc39b97
Iuzziel <Iuzziel@users.noreply.github.com>
2017-07-08 20:54:41 +0200

    Update Principale.java

--- FormationSwing/Chronometre/src/application/Principale.java
+++ FormationSwing/Chronometre/src/application/Principale.java
@@ -1,11 +1,11 @@
-package application;		
+package application;
 
-import fenetres.FenetrePrincipale;		
- 		
-public class Principale{		
- 	//*********************************Début de la Main*********************************//		
- 	public static void main (String [] args){		
- 		FenetrePrincipale fenetre = new FenetrePrincipale();		
- 		fenetre.setVisible(true);		
- 	}		
- }
+import fenetres.FenetrePrincipale;
+
+public class Principale{
+	//*********************************Début de la Main*********************************//
+	public static void main (String [] args){
+		FenetrePrincipale fenetre = new FenetrePrincipale();
+		fenetre.setVisible(true);
+	}
+}
